f0cfac923557d456dc360bffefe164056f63599f
dev <nishantb21@gmail.com>
2017-06-09 13:17:04 -0700

    Made the functions extract only for those restaurants for which menus exist and adapted for the https change

--- Utilities/GetMenu.py
+++ Utilities/GetMenu.py
@@ -3,29 +3,38 @@ import requests
 import jsbeautifier
 import pprint
 
-req_url = 'http://eventshop.ics.uci.edu:8004/search/restaurant/location?lat=33.645864&lon=-117.842787&rad=2'
+# To suppress Insecure warnings - to be dealt with later
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+req_url = 'https://eventshop.ics.uci.edu:8004/search/restaurant/location?lat=33.645864&lon=-117.842787&rad=2'
 auth = ('INDIA','SHINING')
 options = jsbeautifier.default_options()
 options.brace_style = 'expand'
 
 def getRestaurantIDs(req_url):
 	restID_list = []
-	r = requests.get(req_url, auth=auth)
+	# Just a hack to get past the verification
+	r = requests.get(req_url, auth=auth,verify=False)
 	record = json.loads(r.text)
 	for i in record:
 		restID_list.append((i['restaurantName'],i['restaurantId']))
-		#print(i,'\n')	
-	#print i['restaurantId']
 	return restID_list
 
 restID_list = getRestaurantIDs(req_url)
-test_list = [('something','cf40c45d-0160-410a-892a-cbc78011de8a')]
 
 def getMenus(restID_list):
+	without_menus = 0
 	for i in restID_list:
-		menu_url = 'http://eventshop.ics.uci.edu:8004/restaurant/'+ i[1] +'/menu'
-		r = requests.get(menu_url,auth=auth)
-		with open('Menus/'+ i[0].encode('utf8') +'.json','w') as outfile:
-			outfile.write(jsbeautifier.beautify(r.text,opts=options))
-		print(i[0].encode('utf8'))
+		menu_url = 'https://eventshop.ics.uci.edu:8004/restaurant/'+ i[1] +'/menu'
+		# Just a hack to get past verification
+		r = requests.get(menu_url,auth=auth,verify=False)
+		if (r.text == "[]"):
+			without_menus += 1
+		else:
+			print(i[0] + ',' + i[1])
+			with open('Menus/'+ i[0].encode('utf8') +'.json','w') as outfile:
+				outfile.write(jsbeautifier.beautify(r.text,opts=options))
+	return without_menus
+
 getMenus(restID_list)
