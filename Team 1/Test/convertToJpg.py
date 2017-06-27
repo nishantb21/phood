1474ab31954539310a318abb0376437179a1bf8f
Kushal Mehta <kushalmehta13@gmail.com>
2017-06-27 11:16:41 -0700

    convert to jpg now works recursively

--- Team 1/Test/convertToJpg.py
+++ Team 1/Test/convertToJpg.py
@@ -3,14 +3,17 @@ import os
 import imghdr
 from PIL import Image
 
-for item in os.listdir("."):
-    if(imghdr.what(item)=="jpeg"):
-        os.rename(item,item+".jpg")
-    else:
-        try:
-            im=Image.open(item)
-            im.convert('RGB').save(item + ".jpg","JPEG")
-            im.close()
-            os.remove(item)
-        except Exception as e:
-            print("Error:",e)
+root = '.'
+for path, subdirs, files in os.walk(root):
+    for name in files:
+        item = os.path.join(path, name)
+        if(imghdr.what(item)=="jpeg"):
+            os.rename(item,item+".jpg")
+        else:
+            try:
+                im=Image.open(item)
+                im.convert('RGB').save(item + ".jpg","JPEG")
+                im.close()
+                os.remove(item)
+            except Exception as e:
+                print("Error:",e)
