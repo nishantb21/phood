70983cf37279f835d15b97b3a9dd5dc78db9e843
JazzType <akash.kulhalli@gmail.com>
2017-07-05 14:05:17 -0700

    Handled singular forms

--- Team3/datak.py
+++ Team3/datak.py
@@ -17,34 +17,39 @@ def leech(for_file):
 			print('\rQuerying for: ' + query.strip(), end='', flush=True)
 			response = requests.get(query_url + parameterize(query))
 			response = response.json()
-			if len(response['common']) > 0:
-				response = response['common'][0]
-				title_hash = hashlib.md5(response['food_name'].upper().encode('utf-8')).hexdigest()
-				if not os.path.exists(os.path.join('nutritionix_data', title_hash)):
-					response_nutrition = requests.post(nutrition_common_url, data = {'query': response['food_name']})
-					
-					try:
-						response_nutrition = response_nutrition.json()
-						response_nutrition = response_nutrition['foods'][0]
-					except json.decoder.JSONDecodeError:
-						print(': ', response_nutrition.text, sep='')
-						print('-'*15)
+			try:
+				if len(response['common']) > 0:
+					response = response['common'][0]
+					title_hash = hashlib.md5(response['food_name'].upper().encode('utf-8')).hexdigest()
+					if not os.path.exists(os.path.join('nutritionix_data', title_hash)):
+						response_nutrition = requests.post(nutrition_common_url, data = {'query': response['food_name']})
+						
+						try:
+							response_nutrition = response_nutrition.json()
+							response_nutrition = response_nutrition['foods'][0]
+						except json.decoder.JSONDecodeError:
+							print(': ', response_nutrition.text, sep='')
+							print('-'*15)
 
-					print(', got ', response['food_name'], end=', ', sep='', flush=True)
-					matched = [word for word in query.strip().split(' ') if word in response['food_name'].upper().split(' ')]
-					#print(matched, end=' : ')					
+						print(', got ', response['food_name'], end=', ', sep='', flush=True)
+						matched = [word for word in query.strip().split(' ') if word in response['food_name'].upper().split(' ')]
+						#print(matched, end=' : ')					
 
-					if len(matched)/len(response['food_name'].split(' ')) < 0.4:
-						print(len(matched)/len(response['food_name'].split(' ')))
-						print('discarded', end='*'*7)
-						discards.write(str((query.strip(), response['food_name'])) + '\n')
-					else:
-						if len(response['food_name'].split(' ')) == 1 and query.strip() in response['food_name']:							
-							with open(os.path.join('nutritionix_data', title_hash), "w") as ing_file:
-								json.dump(response_nutrition, ing_file)
-						elif len(matched) > 0:
-							with open(os.path.join('nutritionix_data', title_hash), "a") as ing_file:
-								json.dump(response_nutrition, ing_file)
-			else:
-				misses.write(query)
+						if ' ' in response['food_name'] and len(matched)/len(response['food_name'].split(' ')) < 0.4:
+							print(len(matched)/len(response['food_name'].split(' ')))
+							print('discarded', end='*'*7)
+							discards.write(str((query.strip(), response['food_name'])) + '\n')
+						else:
+							if ' ' not in response['food_name'] and query.strip() in response['food_name'].upper():				
+								print(query.strip(), response['food_name'])
+								with open(os.path.join('nutritionix_data', title_hash), "w") as ing_file:
+									json.dump(response_nutrition, ing_file)
+							elif len(matched) > 0:
+								with open(os.path.join('nutritionix_data', title_hash), "a") as ing_file:
+									json.dump(response_nutrition, ing_file)
+				else:
+					misses.write(query)
+			except KeyError as ke:
+				print(ke)
+				print(response.text)
 	print('\nDone.')
