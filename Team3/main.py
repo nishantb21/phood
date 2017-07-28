a4603c1abba9eb12020f51e64c05224606bbffbb
JazzType <akash.kulhalli@gmail.com>
2017-07-27 18:31:40 -0700

    Integrated collective nutrition logic

--- Team3/main.py
+++ Team3/main.py
@@ -13,15 +13,14 @@ arguments = parser.parse_args()
 if arguments.profile:
 	for dish in arguments.profile:
 		nutrition = dict()
-		# Call stop word scrubber here
-		# add_sides(subtitle_list, main_title, save_to_file=True/False)
 		if os.path.exists(utilities.hash(dish) + ".json"):
 			with open(utilities.hash(dish) + ".json") as ifile:
 				nutrition = json.load(ifile)
 		else:
 			nutrition = datak.ingredient(dish)
 			if not nutrition["item_data"].lower() == dish.lower():
-				print(nutrition["item_name"], file=sys.stderr)
-			nutrition = nutrition.nutrition_data
+				subtitle_list = utilities.split_title(dish)
+				print(subtitle_list)
+				nutrition = utilities.add_sides(subtitle_list, dish, save_to_file=True)
 		tastejson = taster.taste_dish(dish, nutrition)
 		print(json.dumps(tastejson, sort_keys=True))
