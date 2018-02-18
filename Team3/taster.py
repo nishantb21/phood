27d1a953f85858a72ee9f43ac8d64f79bf51f5c7
JazzType <akash.kulhalli@gmail.com>
2018-02-18 14:33:22 +0530

    Updated required data files

--- Team3/taster.py
+++ Team3/taster.py
@@ -1,7 +1,7 @@
 import json
-import sys
+import re
 import utilities
-import itertools
+import argparse
 
 SWEET_FACTOR_X = 0.9
 SWEET_FACTOR_Y = 0.1
@@ -16,150 +16,160 @@ SOURNESS_FACTOR_Z = 0.5
 
 
 def sweet(nutrition_data, SWEET_FACTOR_X=0.85, SWEET_FACTOR_Y=0.1):
-	for key in nutrition_data.keys():
-		if nutrition_data[key] is None:
-			nutrition_data[key] = 0
 	try:
-		total_weight = nutrition_data['metric_qty']
-		sweet_score_x1 = abs(nutrition_data['sugars'] - nutrition_data['dietary_fiber']) / total_weight
-		sweet_score_y = nutrition_data['sugars'] / nutrition_data['total_carb']
+		total_weight = nutrition_data['Weight']
+		fibtg = 0
+		if 'Dietary_Fiber' in nutrition_data:
+				fibtg = nutrition_data['Dietary_Fiber']
+		sweet_score_x1 = abs(nutrition_data['Sugar'] - fibtg) / total_weight
+		sweet_score_y = nutrition_data['Sugar'] / nutrition_data['Carbs']
 		sweet_score_1 = (SWEET_FACTOR_X * sweet_score_x1) + (SWEET_FACTOR_Y * sweet_score_y)
+		#	print(sweet_score_1)
 	except Exception:
 		sweet_score_1 = 0
-	return round(sweet_score_1 * 10 / 0.998, 3)
+	return round(sweet_score_1 / 0.998,3) * 10
 
 
-def rich(nutrition_data, RICHNESS_FACTOR_X=0.2, RICHNESS_FACTOR_Y=1.3, RICHNESS_FACTOR_Z=50):
-	for key in nutrition_data.keys():
-		if nutrition_data[key] is None:
-			nutrition_data[key] = 0
+def rich(nutrition_data, RICHNESS_FACTOR_X=0.5, RICHNESS_FACTOR_Y=0.7,RICHNESS_FACTOR_Z=50):
 	try:
-		total_weight = nutrition_data['metric_qty']
-		richness_score_x = nutrition_data['saturated_fat'] / nutrition_data['total_fat']  # high
-		richness_score_y = nutrition_data['total_fat'] / total_weight  # low
-		richness_score_z = nutrition_data['cholesterol'] / (total_weight * 1000)
+		#total_weight = nutrition_data['metric_qty']
+		total_weight = nutrition_data['Weight']
+		richness_score_x = 0
+		if 'Sat_Fat' in nutrition_data:
+			richness_score_x = nutrition_data['Sat_Fat'] / nutrition_data['Fat'] #high
+		#Why consider sat_fat if we're considering total fat anyway? Would make more sense if total fat wasn't available
+		richness_score_y = nutrition_data['Fat'] / total_weight #low
+		#Why is this not enough to work off of?
+		richness_score_z = 0
+		if 'Cholesterol' in nutrition_data:
+			richness_score_z = nutrition_data['Cholesterol'] / (total_weight * 1000)
+		#I'll admit that this is a good idea anyway
 		richness_score_1 = (RICHNESS_FACTOR_X * richness_score_x) + (RICHNESS_FACTOR_Y * richness_score_y) + (RICHNESS_FACTOR_Z * richness_score_z)
 	except Exception:
 		richness_score_1 = 0
 
-	return round((richness_score_1 * 10 / 0.992), 3)  # Normalize to butter which has highest score
-
-
-def sour(dish_title, nutrition_data, SOURNESS_FACTOR_X=0.1, SOURNESS_FACTOR_Y=0.25, SOURNESS_FACTOR_Z=0.5):
-	total_weight = nutrition_data['total_carb'] + nutrition_data['protein'] + nutrition_data['total_fat']
-	food_words = dish_title.upper().split(' ')
-
-	try:
-		vitamin_c = nutrition_data['vitamin_c']
-	except KeyError:
-		vitamin_c = 0.0
-
-	with open('sour.json') as f:
-		sour = json.load(f)
-
-	with open('too_sour.json') as f:
-		too_sour = json.load(f)
-	try:
-		sour_score_x = vitamin_c / total_weight
-	except ZeroDivisionError:
-		sour_score_x = 0
-
-	sour_score_y = 0
-	sour_score_z = 0
-
-	for word in food_words:
-		if word in sour[word[0]]:
-			sour_score_y += 1
-		if word in too_sour[word[0]]:
-			sour_score_z += 1
-	sour_score = round(((SOURNESS_FACTOR_X * sour_score_x) + (SOURNESS_FACTOR_Y * sour_score_y) + (SOURNESS_FACTOR_Z * sour_score_z)) / 0.995, 3)
-	if sour_score > 1:
-		sour_score = 1
-	return round(sour_score * 10, 3)
-
-
-def spicy(dish_title):
-	food_words = dish_title.upper().split(' ')
-	with open('spicy.json') as f:
-		spice = json.load(f)
-	for word in food_words:
-		if word in spice[word[0]]:
-			return 1
-	return 0
-
-
-def match_descriptors(dish_title, descriptor_dict):
-	dish_split = dish_title.split(" ")
-	final_scores = dict()
-	for item in descriptor_dict:
-		for pair in itertools.product(item['items'].items(), dish_split):
-			if pair[0][0].lower() in pair[1].lower().strip():
-				try:
-					final_scores[item["name"]] += pair[0][1]
-				except KeyError:
-					final_scores[item["name"]] = pair[0][1]
-	return final_scores
-
-
-def umami(dish_title, nutrition_data, PROTEIN_SUPPLEMENT_MULTIPLIER=0.80, VEGETABLES_MULTIPLIER=10, MEAT_MULTIPLIER=10, STRING_MULTIPLIER=9.45):
-	for key in nutrition_data.keys():
-		if nutrition_data[key] is None:
-			nutrition_data[key] = 0
-
-	umami_descriptors = utilities.read_json("umami_descriptors.json")
-	descriptor_score = match_descriptors(dish_title, umami_descriptors)
-
-	umamiscore = nutrition_data["protein"] / total_weight(nutrition_data)
-	umamiscore *= 10
-
-	pairings = zip([PROTEIN_SUPPLEMENT_MULTIPLIER, VEGETABLES_MULTIPLIER, MEAT_MULTIPLIER, STRING_MULTIPLIER], ["protein_supps", "vegetables", "meat", "savory_strings"])
-	for pair in pairings:
-		if descriptor_score.__contains__(pair[1]):
-			umamiscore += pair[0] * descriptor_score[pair[1]]
-
-	return round(umamiscore, 3) if umamiscore <= 10 else 10
-
-
-def total_weight(dish_nutrition):
-	return dish_nutrition['total_fat'] + dish_nutrition['total_carb'] + dish_nutrition['protein']
+	return round((richness_score_1 / 0.992), 3) * 10
+	## Normalize to butter which has highest score
 
 
 def salt(dish_nutrition):
-	for key in dish_nutrition.keys():
-		if dish_nutrition[key] is None:
-			dish_nutrition[key] = 0
-	totalweight = total_weight(dish_nutrition)
+	totalweight = dish_nutrition['Weight']
 	if totalweight == 0:
-		return dish_nutrition['sodium'] / dish_nutrition['metric_qty'] / 3.8758
-	return round((dish_nutrition['sodium'] / totalweight) / 3.8758, 3)
+		return 0
+	#return((dish_nutrition['sodium'] / dish_nutrition['metric_qty']))
+	#print((dish_nutrition['sodium'] / dish_nutrition['metric_qty']) / 38.758, end=' | ')
+	return ((1000 * dish_nutrition['Sodium'] / totalweight)) / 3.8758
 
 
-def bitter(dish_title, nutrition_data, LEVEL1_MULTIPLIER=0.80, LEVEL2_MULTIPLIER=1.40, MULTI_WORD_MULTIPLIER=2.3):
-	bitter_descriptors = utilities.read_json("bitter_descriptors.json")
-	descriptor_score = match_descriptors(dish_title, bitter_descriptors)
-	bitterscore = nutrition_data["iron"] / total_weight(nutrition_data)
-	pairings = zip([LEVEL1_MULTIPLIER, LEVEL2_MULTIPLIER, MULTI_WORD_MULTIPLIER], ["bitter_l1", "bitter_l2", "multi_words"])
-	for pair in pairings:
-		if descriptor_score.__contains__(pair[1]):
-			bitterscore += pair[0] * descriptor_score[pair[1]] * 1
-	return round(bitterscore / 1.4571, 3)
+def get_dishes():
+	with open('testset.json') as json_file:
+		foods_list = json.load(json_file)
+
+	return foods_list
 
 
-def taste_dish(dish_title, jv):
-	return {
-		"sweet": sweet(jv),
-		"rich": rich(jv),
-		"sour": sour(dish_title, jv),
-		"spicy": spicy(dish_title),
-		"umami": umami(dish_title, jv),
-		"bitter": bitter(dish_title, jv),
-		"salt": salt(jv)
-	}
+def total_weight(dish_nutrition):
+	totalweight = 0
+	for nutrient in dish_nutrition:
+		#print(dish_nutrition[nutrient])
+		if dish_nutrition[nutrient] is not None and 'g' in dish_nutrition[nutrient][0]:
+			#print(nutrient,dish_nutrition[nutrient])
+			number = re.findall('(\d+\.\d+|\d+)', dish_nutrition[nutrient][0])
+			#print(nutrient,dish_nutrition[nutrient][0],number)
+			#print(numpy)
+			numeric_value = 0
+			if len(number) > 0:
+				numeric_value = float(number[0])
+			totalweight += numeric_value
+			#print(totalweight)
+	return totalweight
 
 
-if __name__ == "__main__":
-	for file in sys.argv[1:]:
-		with open(file) as infile:
-			jv = json.load(infile)
-			result = taste_dish(jv)
-			print(file, result)
+def get_nutrients(food):
+	nutrients = food['nutrients']
+	totalweight = total_weight(nutrients)
+	#print(total_weight)
+	for nutrient in nutrients:
+		if nutrients[nutrient] is not None:
+			number = re.findall('\d+\.\d+', nutrients[nutrient][0])
+			if len(number) > 0:
+				nutrients[nutrient] = float(number[0])
+			else:
+				nutrients[nutrient] = 0
+	nutrients['Weight'] = totalweight
+	return nutrients
+
+
+def taste(food):
+		taste_scores = dict()
+		nutrients = get_nutrients(food)
+		salt_score = salt(nutrients)
+		taste_scores['salt'] = salt_score
+		sweet_score = sweet(nutrients)
+		taste_scores['sweet'] = sweet_score
+		richness_score = rich(nutrients)
+		taste_scores['rich'] = richness_score
+		tags = get_cuisine_tags(food)
+		cuisine_multipliers = get_cuisine_multipliers(tags)
+		taste_scores = update_scores(taste_scores,cuisine_multipliers)
+		#taste_scores = cuisine_taste(taste_scores)
+		return taste_scores
+
+
+def update_scores(taste_scores, cuisine_multipliers):
+	print(cuisine_multipliers)
+	for taste in taste_scores:
+		taste_scores[taste] = taste_scores[taste] * cuisine_multipliers[taste]
+	return taste_scores
+
+
+def get_cuisine_multipliers(tags):
+	with open('cuisine_multipliers.json') as json_file:
+		cuisine_multipliers = json.load(json_file)
+	default =  {
+		"salt": 1.0,
+		"sweet": 1.0,
+		"rich": 1.0
+		}
+	if tags is not None:
+		if len(tags) == 0:
+			return default
+		if len(tags) == 1:
+			return cuisine_multipliers[tags[0]]
+		elif len(tags) == 2:
+			return cuisine_multipliers[tags[0]][tags[1]]
+	else:
+		return default
+
+
+def get_cuisine_tags(food):
+	with open('first_50_tags.json') as json_file:
+		tags = json.load(json_file)
+	closest_match = utilities.modmatchi(food['dish_name'],list(tags),threshold=0.5)
+	#print(food['dish_name'],closest_match)
+	if closest_match[0] is not None:
+		return tags[closest_match[0]]
+
+
+def main():
+	foods_list = get_dishes()
+	for food in foods_list:
+		#if food['dish_id'] == 98:
+		print(food['dish_name'], taste(food))
+
+
+if __name__ == '__main__':
+	# main()
+	parser = argparse.ArgumentParser(prog="Fabric - Taste Profiler")
+	parser.add_argument("-f", "--file", help="Load the dish from the file specified", action='append')
+	parser.add_argument("-p", "--profile", help="Profiles the JSON", action='append')
+	arguments = parser.parse_args()
+
+	if arguments.file:
+		for file in arguments.file:
+			with open(file) as inputfile:
+				print(json.dumps(taste(json.load(inputfile)), sort_keys=True))
+
+	if arguments.profile:
+		for jsonarg in arguments.profile:
+			print(json.dumps(taste(json.loads(jsonarg)), sort_keys=True))
--- allrecipes_leech.py
+++ allrecipes_leech.py
@@ -6,7 +6,7 @@ import urllib
 
 
 def has_next_page(responsejson):
-	return responsejson['links'].__contains__("next")
+  return responsejson['links'].__contains__("next")
 
 
 '''
@@ -16,63 +16,64 @@ def has_next_page(responsejson):
 
 
 def leech(recipeid):
-	url = 'https://apps.allrecipes.com/v1/recipes/{}/reviews/'
-	headers = {
-		'Authorization': 'Bearer 84XAr4Z2Z57+ieH6qwuIreIELMkV8r3bTThXeMCEIvPul4qNGScAVlMMnBYtubd+6quob+PJ14hTpMrHWyL4gkvpaO77IQRILXaivMT11VYAiJquc/3WzdKtZO2HP93a+Pl4j9uPRbuvovewunh4uLO3xhwTu4dESHag0OlqjgssrL7lnP7wfw=='
-	}
-	queryparams = {
-		"pagesize": 100,
-		"page": 1
-	}
+  url = 'https://apps.allrecipes.com/v1/recipes/{}/reviews/'
+  headers = {
+      'Authorization': 'Bearer 84XAr4Z2Z57+ieH6qwuIreIELMkV8r3bTThXeMCEIvPul4qNGScAVlMMnBYtubd+6quob+PJ14hTpMrHWyL4gkvpaO77IQRILXaivMT11VYAiJquc/3WzdKtZO2HP93a+Pl4j9uPRbuvovewunh4uLO3xhwTu4dESHag0OlqjgssrL7lnP7wfw=='
+  }
+  queryparams = {
+      "pagesize": 100,
+      "page": 1
+  }
 
-	response = requests.get(
-		url.format(recipeid),
-		headers=headers,
-		params=queryparams)
-	responsejson = response.json()
+  response = requests.get(
+      url.format(recipeid),
+      headers=headers,
+      params=queryparams)
+  responsejson = response.json()
 
-	print(
-		'GET Recipe ID:{:6} Page {:2} - {:3} {}'.format(
-			recipeid, 1, response.status_code, response.reason),
-		end='\r')
+  print(
+      'GET Recipe ID:{:6} Page {:2} - {:3} {}'.format(
+          recipeid, 1, response.status_code, response.reason),
+      end='\r')
 
-	reviews = list()
+  reviews = list()
 
-	while has_next_page(responsejson):
-		pageno = int(
-			urllib.parse.parse_qs(
-				urllib.parse.urlparse(
-					responsejson["links"]["next"]["href"]).query)['page'][0])
-		print('GET Recipe ID:{:6} Page {:2} - {:3} {}'.format(
-			recipeid, pageno, response.status_code, response.reason), end='\r')
+  while has_next_page(responsejson):
+    pageno = int(
+        urllib.parse.parse_qs(
+            urllib.parse.urlparse(
+                responsejson["links"]["next"]["href"]).query)['page'][0])
+    print('GET Recipe ID:{:6} Page {:2} - {:3} {}'.format(
+        recipeid, pageno, response.status_code, response.reason), end='\r')
 
-		reviews.extend(responsejson["reviews"])
-		queryparams["page"] = pageno
-		response = requests.get(
-			url.format(recipeid),
-			headers=headers,
-			params=queryparams)
+    reviews.extend(responsejson["reviews"])
+    queryparams["page"] = pageno
+    response = requests.get(
+        url.format(recipeid),
+        headers=headers,
+        params=queryparams)
 
-		responsejson = response.json()
+    responsejson = response.json()
 
-	reviews.extend(responsejson["reviews"])
+  reviews.extend(responsejson["reviews"])
 
-	if not os.path.exists("recipereviews"):
-		os.mkdir("recipereviews")
+  if not os.path.exists("recipereviews"):
+    os.mkdir("recipereviews")
 
-	with open(
-		os.path.join("recipereviews", "{}.json".format(recipeid)),
-		"w") as outfile:
-		json.dump(reviews, outfile, indent='\t')
+  with open(
+          os.path.join("recipereviews", "{}.json".format(recipeid)),
+          "w") as outfile:
+    json.dump(reviews, outfile, indent='\t')
 
-	print("~ Recipe ID {:6}: Wrote {:4} reviews ~".format(recipeid, len(reviews)))
+  print("~ Recipe ID {:6}: Wrote {:4} reviews ~".format(
+      recipeid, len(reviews)))
 
-	return reviews
+  return reviews
 
 
 if __name__ == "__main__":
-	if len(sys.argv) > 1:
-		for recipeid in sys.argv[1:]:
-			leech(int(recipeid))
-	else:
-		leech(68461)
+  if len(sys.argv) > 1:
+    for recipeid in sys.argv[1:]:
+      leech(int(recipeid))
+  else:
+    leech(68461)
